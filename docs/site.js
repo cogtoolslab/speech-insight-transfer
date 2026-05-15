c294b8e43f2a885df591eee9ccf3c67564ca990e
LinasNas <linasmnasvytis@gmail.com>
2026-05-15 12:09:49 -0700

    update website

--- docs/site.js
+++ docs/site.js
@@ -13,13 +13,19 @@ document.querySelectorAll("[data-example]").forEach((card) => {
     if (showingSolution) {
       image.setAttribute("src", image.dataset.problem);
       label.hidden = true;
+      button.classList.add("show-solution");
       button.setAttribute("aria-expanded", "false");
       button.textContent = "Show Solution";
     } else {
       image.setAttribute("src", image.dataset.solution);
       label.hidden = false;
+      button.classList.remove("show-solution");
       button.setAttribute("aria-expanded", "true");
       button.textContent = "Show Problem";
     }
   });
+});
+
+document.querySelectorAll(".reveal-button").forEach((button) => {
+  button.classList.add("show-solution");
 });
